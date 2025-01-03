fb53369b1040c84b2a5c865aa0067e473b115c64
Smoliarenko Alex <smoliarenko.sasha@gmail.com>
2025-01-03 16:10:14 +0200

    add functionality for cart and favorite features, update styles and IDs for photo selection

--- js/phone.js
+++ js/phone.js
@@ -101,4 +101,144 @@ window.onload = () => {
 
     const savedColor = localStorage.getItem('color');
     updateColor(savedColor === 'black' || savedColor === 'natural' || savedColor === 'green' || savedColor === 'pink' || savedColor === 'gold' || savedColor === 'blue' ? savedColor : phone.color);
+
+    const savedInCart = localStorage.getItem('inCart');
+    phone.inCart = savedInCart === 'true';
+    addToCart.classList.toggle('inCart', phone.inCart);
+    addToCart.innerHTML = phone.inCart
+        ? `Прибрати з кошика <svg width="27" height="27">
+                  <use
+                    href="./img/symbol-defs.svg#icon-buy"
+                    style="fill: #fff"
+                  ></use>
+                </svg>`
+        : `Додати у кошик <svg width="27" height="27">
+                  <use
+                    href="./img/symbol-defs.svg#icon-buy"
+                    style="fill: #fff"
+                  ></use>
+                </svg>`;
+    const savedFavourite = localStorage.getItem('favourite');
+    phone.favourite = savedFavourite === 'true';
+    addToFavourite.classList.toggle('favourite', phone.favourite);
+    addToFavourite.innerHTML = phone.favourite
+        ? `<svg width="27" height="27">
+                  <use
+                    href="./img/symbol-defs.svg#icon-heart"
+                    style="fill: red"
+                  ></use>
+                </svg> Видалити з обраного `
+        : `<svg width="27" height="27">
+                  <use
+                    href="./img/symbol-defs.svg#icon-heart"
+                    style="fill: red"
+                  ></use>
+                </svg> Додати в обране `;
+    const savedPhoto = localStorage.getItem('photo');
+    phone.photo = savedPhoto === 'photo1' || savedPhoto === 'photo2' || savedPhoto === 'photo3' || savedPhoto === 'photo4' ? savedPhoto : phone.photo;
+    mainPhoto.classList.remove('photo1', 'photo2', 'photo3', 'photo4');
+    mainPhoto.classList.add(phone.photo);
+    if (phone.photo === 'photo1') {
+        photo1.classList.add('photo-active');
+    } else if (phone.photo === 'photo2') {
+        photo2.classList.add('photo-active');
+    } else if (phone.photo === 'photo3') {
+        photo3.classList.add('photo-active');
+    } else if (phone.photo === 'photo4') {
+        photo4.classList.add('photo-active');
+    }
 };
+
+const addToCart = document.querySelector('#addToCart');
+const addToFavourite = document.querySelector('#addToFavourite');
+
+addToCart.addEventListener('click', () => {
+    phone.inCart = !phone.inCart;
+    if (phone.inCart) {
+        addToCart.innerHTML = `Прибрати з кошика <svg width="27" height="27">
+                  <use
+                    href="./img/symbol-defs.svg#icon-buy"
+                    style="fill: #fff"
+                  ></use>
+                </svg>`;
+    } else {
+        addToCart.innerHTML = `Додати у кошик <svg width="27" height="27">
+                  <use
+                    href="./img/symbol-defs.svg#icon-buy"
+                    style="fill: #fff"
+                  ></use>
+                </svg>`;
+    }
+
+    addToCart.classList.toggle('inCart', phone.inCart);
+
+    localStorage.setItem('inCart', phone.inCart);
+});
+
+addToFavourite.addEventListener('click', () => {
+    phone.favourite = !phone.favourite;
+    if (phone.favourite) {
+        addToFavourite.innerHTML = `<svg width="27" height="27">
+                  <use
+                    href="./img/symbol-defs.svg#icon-heart"
+                    style="fill: red"
+                  ></use>
+                </svg> Видалити з обраного `;
+    } else {
+        addToFavourite.innerHTML = `<svg width="27" height="27">
+                  <use
+                    href="./img/symbol-defs.svg#icon-heart"
+                    style="fill: red"
+                  ></use>
+                </svg> Додати в обране `;
+    }
+    addToFavourite.classList.toggle('favourite', phone.favourite);
+    localStorage.setItem('favourite', phone.favourite);
+});
+
+const photo1 = document.querySelector('#photo1');
+const photo2 = document.querySelector('#photo2');
+const photo3 = document.querySelector('#photo3');
+const photo4 = document.querySelector('#photo4');
+const mainPhoto = document.querySelector('#mainPhoto');
+
+photo1.addEventListener('click', () => {
+    phone.photo = 'photo1';
+    localStorage.setItem('photo', phone.photo);
+    mainPhoto.classList.remove('photo2', 'photo3', 'photo4');
+    mainPhoto.classList.add('photo1');
+    photo1.classList.add('photo-active');
+    photo2.classList.remove('photo-active');
+    photo3.classList.remove('photo-active');
+    photo4.classList.remove('photo-active');
+});
+photo2.addEventListener('click', () => {
+    phone.photo = 'photo2';
+    localStorage.setItem('photo', phone.photo);
+    mainPhoto.classList.remove('photo1', 'photo3', 'photo4');
+    mainPhoto.classList.add('photo2');
+    photo2.classList.add('photo-active');
+    photo1.classList.remove('photo-active');
+    photo3.classList.remove('photo-active');
+    photo4.classList.remove('photo-active');
+});
+photo3.addEventListener('click', () => {
+    phone.photo = 'photo3';
+    localStorage.setItem('photo', phone.photo);
+    mainPhoto.classList.remove('photo1', 'photo2', 'photo4');
+    mainPhoto.classList.add('photo3');
+    photo3.classList.add('photo-active');
+    photo1.classList.remove('photo-active');
+    photo2.classList.remove('photo-active');
+    photo4.classList.remove('photo-active');
+});
+photo4.addEventListener('click', () => {
+    phone.photo = 'photo4';
+    localStorage.setItem('photo', phone.photo);
+    mainPhoto.classList.remove('photo1', 'photo2', 'photo3');
+    mainPhoto.classList.add('photo4');
+    photo4.classList.add('photo-active');
+    photo1.classList.remove('photo-active');
+    photo2.classList.remove('photo-active');
+    photo3.classList.remove('photo-active');
+});
